3e1dda4da1a1aea9eece496ec00afaa9e4ae9edd
潘金鑫_北京微智信业 <panjx@mvtech.com.cn>
2018-12-26 15:54:21 +0800

    优化整改退回后提交

--- src/main/java/cn/hse/util/DateUtil.java
+++ src/main/java/cn/hse/util/DateUtil.java
@@ -77,7 +77,7 @@ public final class DateUtil {
      * @return
      */
     public static Date string2Date(String date) {
-        SimpleDateFormat format = new SimpleDateFormat(DatePattern.YYYY_MM_DD.getContext());
+        SimpleDateFormat format = new SimpleDateFormat(DatePattern.YYYY_MM_DD_HH_MM_SS.getContext());
         try {
             return format.parse(date);
         } catch (ParseException e) {
